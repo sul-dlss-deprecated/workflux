a095b27b197424962c34931941f912246f0733cb
Joe Atzberger <ohiocore@gmail.com>
2016-02-10 18:34:23 -0800

    Regenerated schema (names, order, migration)

--- db/schema.rb
+++ db/schema.rb
@@ -9,28 +9,48 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# To stand in this app as a separate interface to the workflows database,
-# we will need some aliases to maintain Rails naming conventions (plural table names).
-#
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "archived_statuses", force: :cascade do |t|
+    t.string   "druid",      limit: 256,                                              null: false
+    t.string   "datastream", limit: 256,                                              null: false
+    t.string   "process",    limit: 256,                                              null: false
+    t.string   "status",     limit: 256
+    t.string   "error_msg",  limit: 4000
+    t.text     "error_txt"
+    t.datetime "datetime"
+    t.integer  "attempts",                                        default: 0
+    t.string   "lifecycle",  limit: 256
+    t.decimal  "elapsed",                 precision: 9, scale: 3
+    t.string   "repository", limit: 256
+    t.datetime "archive_dt",                                                          null: false
+    t.integer  "version",                                         default: 1
+    t.string   "note",       limit: 4000
+    t.integer  "priority",                                        default: 0
+    t.string   "lane_id",    limit: 256,                          default: "default", null: false
+  end
+
+  add_index "archived_statuses", ["datastream"], name: "ds_wf_ar_bitmap_idx"
+  add_index "archived_statuses", ["druid"], name: "druid_wf_ar_idx"
+  add_index "archived_statuses", ["repository"], name: "repo_wf_ar_bitmap_idx"
 
   create_table "statuses", force: :cascade do |t|
-    t.string    "druid",      limit: 256,                                               null: false
-    t.string    "datastream", limit: 256,                                               null: false
-    t.string    "process",    limit: 256,                                               null: false
-    t.string    "status",     limit: 256
-    t.string    "error_msg",  limit: 4000
-    t.text      "error_txt"
-    t.timestamp "datetime",   limit: 6
-    t.integer   "attempts",                precision: 38,           default: 0
-    t.decimal   "elapsed",                 precision: 9,  scale: 3
-    t.string    "lifecycle",  limit: 256
-    t.string    "repository", limit: 256
-    t.string    "note",       limit: 4000
-    t.integer   "priority",                precision: 38,           default: 0
-    t.string    "lane_id",    limit: 256,                           default: "default", null: false
+    t.string   "druid",      limit: 256,                                              null: false
+    t.string   "datastream", limit: 256,                                              null: false
+    t.string   "process",    limit: 256,                                              null: false
+    t.string   "status",     limit: 256
+    t.string   "error_msg",  limit: 4000
+    t.text     "error_txt"
+    t.datetime "datetime"
+    t.integer  "attempts",                                        default: 0
+    t.decimal  "elapsed",                 precision: 9, scale: 3
+    t.string   "lifecycle",  limit: 256
+    t.string   "repository", limit: 256
+    t.string   "note",       limit: 4000
+    t.integer  "priority",                                        default: 0
+    t.string   "lane_id",    limit: 256,                          default: "default", null: false
   end
 
   add_index "statuses", ["datetime", "repository"], name: "tr_wf_idx"
@@ -38,26 +58,4 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "statuses", ["process", "status", "datastream", "lane_id", "repository"], name: "psdlr_wf_idx"
   add_index "statuses", ["repository", "process", "status", "datastream", "druid", "priority"], name: "idx$$_095f0001"
 
-  create_table "archived_statuses", force: :cascade do |t|
-    t.string    "druid",      limit: 256,                                                    null: false
-    t.string    "datastream", limit: 256,                                                    null: false
-    t.string    "process",    limit: 256,                                                    null: false
-    t.string    "status",     limit: 256
-    t.string    "error_msg",  limit: 4000
-    t.text      "error_txt"
-    t.timestamp "datetime",   limit: 6
-    t.integer   "attempts",                precision: 38,           default: 0
-    t.string    "lifecycle",  limit: 256
-    t.decimal   "elapsed",                 precision: 9,  scale: 3
-    t.string    "repository", limit: 256
-    t.timestamp "archive_dt", limit: 6,                             default: "SYSTIMESTAMP", null: false
-    t.integer   "version",                 precision: 38,           default: 1
-    t.string    "note",       limit: 4000
-    t.integer   "priority",                precision: 38,           default: 0
-    t.string    "lane_id",    limit: 256,                           default: "default",      null: false
-  end
-
-  add_index "archived_statuses", ["datastream"], name: "ds_wf_ar_bitmap_idx"
-  add_index "archived_statuses", ["druid"], name: "druid_wf_ar_idx"
-  add_index "archived_statuses", ["repository"], name: "repo_wf_ar_bitmap_idx"
 end
